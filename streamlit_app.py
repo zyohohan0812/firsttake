bbe9e8d37ab6a2b38c95efaaeadbdd3bccfa5b18
zyohohan0812 <144981054+zyohohan0812@users.noreply.github.com>
2023-09-21 04:39:40 +0000

    ラッキーアイテム３

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ if st.button("おみくじを引く"):
   results = ["大吉","中吉","小吉","吉","凶","大凶"]
   result = random.choice(results)
   st.write(f"結果:{result}")
-  if s.button("ラッキーアイテム"):
+  if st.button("ラッキーアイテム"):
     item = ["ダイヤ","ルビー","サファイア","マリア"]
     st.write(f"ラッキーアイテム:{item}")
 
